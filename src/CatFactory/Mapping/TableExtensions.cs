c681e5d8357e9f554a3a1368edaa63b83cd7ca20
HH <hans.herzl@live.com>
2016-12-22 23:59:37 -0500

    Version 1.0.0-alpha-build12: addition of method to retrieve columns without identity

--- src/CatFactory/Mapping/TableExtensions.cs
+++ src/CatFactory/Mapping/TableExtensions.cs
@@ -9,5 +9,10 @@ namespace CatFactory.Mapping
         {
             return table.PrimaryKey == null ? table.Columns : table.Columns.Where(item => !table.PrimaryKey.Key.Contains(item.Name));
         }
+
+        public static IEnumerable<Column> GetColumnsWithOutIdentity(this ITable table)
+        {
+            return table.Identity == null ? table.Columns : table.Columns.Where(item => table.Identity.Name != item.Name);
+        }
     }
 }
