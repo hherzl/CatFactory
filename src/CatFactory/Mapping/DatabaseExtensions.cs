65b3a47dbbcad51a69a07385c283030ba7d3ac36
HH <hans.herzl@live.com>
2017-03-05 01:44:54 -0500

    Version 1.0.0-alpha-build32: addition of extensions methods for retrieve objects from database instance

--- src/CatFactory/Mapping/DatabaseExtensions.cs
+++ src/CatFactory/Mapping/DatabaseExtensions.cs
@@ -1,10 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace CatFactory.Mapping
 {
     public static class DatabaseExtensions
     {
+        public static IEnumerable<DbObject> GetTables(this Database db)
+            => db.DbObjects.Where(item => item.Type == "USER_TABLE");
+
+        public static IEnumerable<DbObject> GetViews(this Database db)
+            => db.DbObjects.Where(item => item.Type == "VIEW");
+
+        public static IEnumerable<DbObject> GetProcedures(this Database db)
+            => db.DbObjects.Where(item => item.Type == "SQL_STORED_PROCEDURE");
+
+        public static IEnumerable<DbObject> GetScalarFunctions(this Database db)
+            => db.DbObjects.Where(item => item.Type == "SQL_SCALAR_FUNCTION");
+
+        public static IEnumerable<DbObject> GetTableFunctions(this Database db)
+            => db.DbObjects.Where(item => item.Type == "SQL_TABLE_VALUED_FUNCTION");
+
         public static void AddPrimaryKeyToTables(this Database database)
         {
             foreach (var table in database.Tables)
@@ -16,7 +32,7 @@ namespace CatFactory.Mapping
             }
         }
 
-        public static void AddColumnForAllTables(this Database database, Column[] columns, params String[] exclusions)
+        public static void AddColumnsForAllTables(this Database database, Column[] columns, params String[] exclusions)
         {
             foreach (var table in database.Tables)
             {
@@ -37,7 +53,7 @@ namespace CatFactory.Mapping
 
         public static void AddColumnForAllTables(this Database database, Column column, params String[] exclusions)
         {
-            AddColumnForAllTables(database, new Column[] { column }, exclusions);
+            AddColumnsForAllTables(database, new Column[] { column }, exclusions);
         }
 
         public static void LinkTables(this Database db)
