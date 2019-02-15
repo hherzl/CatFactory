bae2fdfda647e33878d26f9eb6ef8d6a42dd3965
HH <hans.herzl@live.com>
2019-02-14 22:44:58 -0600

    1.0.0-beta-sun-build17 Version:
    * Addition of actions for Entity in ObjectRelationalMapping namespace
    * Addition of XML comments for GetCamelCase, GetPascalCase and GetSnakeCase methods, courtesy of ewmccarty
    * Addition of GetKebabCase method in NamingConvention, courtesy of Christian-Schiffer
    * Add Xml documentation for Collections namespace

--- CatFactory/Collections/IEnumerableExtensions.cs
+++ CatFactory/Collections/IEnumerableExtensions.cs
@@ -4,20 +4,20 @@ using System.Text;
 namespace CatFactory.Collections
 {
     /// <summary>
-    /// 
+    /// Provides extension methods for IEnumerable interface
     /// </summary>
     public static class IEnumerableExtensions
     {
         /// <summary>
-        /// 
+        /// Gets an instance of <see cref="StringBuilder"/> that contains all content from string collection
         /// </summary>
-        /// <param name="enumerable"></param>
-        /// <returns></returns>
-        public static StringBuilder ToStringBuilder(this IEnumerable<string> enumerable)
+        /// <param name="collection">Collection of <see cref="string"/></param>
+        /// <returns>An instance of <see cref="StringBuilder"/> that contains all strings from enumerator</returns>
+        public static StringBuilder ToStringBuilder(this IEnumerable<string> collection)
         {
             var output = new StringBuilder();
 
-            foreach (var item in enumerable)
+            foreach (var item in collection)
             {
                 output.AppendLine(item);
             }
--- CatFactory/Collections/IListExtensions.cs
+++ CatFactory/Collections/IListExtensions.cs
@@ -5,53 +5,53 @@ using CatFactory.CodeFactory;
 namespace CatFactory.Collections
 {
     /// <summary>
-    /// 
+    /// Provides extension methods for <see cref="IList{T}"/> interface
     /// </summary>
     public static class IListExtensions
     {
         /// <summary>
-        /// 
+        /// Adds an item of <typeparamref name="TItem"/> type
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="list"></param>
-        /// <param name="flag"></param>
-        /// <param name="item"></param>
-        public static void Add<T>(this IList<T> list, bool flag, T item)
+        /// <typeparam name="TItem"><typeparamref name="TItem"/> object</typeparam>
+        /// <param name="list">List</param>
+        /// <param name="flag">Condition</param>
+        /// <param name="item">Item</param>
+        public static void Add<TItem>(this IList<TItem> list, bool flag, TItem item)
         {
             if (flag)
                 list.Add(item);
         }
 
         /// <summary>
-        /// 
+        /// Adds an item of <typeparamref name="TItem"/> type if not exists in list
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="list"></param>
-        /// <param name="item"></param>
-        public static void AddUnique<T>(this IList<T> list, T item)
+        /// <typeparam name="TItem"><typeparamref name="TItem"/> object</typeparam>
+        /// <param name="list">List</param>
+        /// <param name="item">Item</param>
+        public static void AddUnique<TItem>(this IList<TItem> list, TItem item)
         {
             if (!list.Contains(item))
                 list.Add(item);
         }
 
         /// <summary>
-        /// 
+        /// Adds an item of <typeparamref name="TItem"/> type if not exists in list
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="list"></param>
-        /// <param name="flag"></param>
-        /// <param name="item"></param>
-        public static void AddUnique<T>(this IList<T> list, bool flag, T item)
+        /// <typeparam name="TItem"><typeparamref name="TItem"/> object</typeparam>
+        /// <param name="list">List</param>
+        /// <param name="flag">condition</param>
+        /// <param name="item">Item</param>
+        public static void AddUnique<TItem>(this IList<TItem> list, bool flag, TItem item)
         {
             if (flag && !list.Contains(item))
                 list.Add(item);
         }
 
         /// <summary>
-        /// 
+        /// Gets an instance of <see cref="StringBuilder"/> that contains all content from <see cref="IList{ILine}"/> list
         /// </summary>
-        /// <param name="list"></param>
-        /// <returns></returns>
+        /// <param name="list">Implementation of <see cref="IList{T}"/> interface</param>
+        /// <returns>An instance of <see cref="StringBuilder"/> that contains all content from <see cref="IList{ILine}"/> list</returns>
         public static StringBuilder ToStringBuilder(this IList<ILine> list)
         {
             var output = new StringBuilder();
